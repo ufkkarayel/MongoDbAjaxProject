6aa0b41e059ba3a07437c3fe577baa62d14778f1
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-08 20:36:38 +0300

    category ve product nesneleri oluşturuldu

--- MongoDbAjaxProject/DAL/Entities/Category.cs
+++ MongoDbAjaxProject/DAL/Entities/Category.cs
@@ -1,6 +1,13 @@
-﻿namespace MongoDbAjaxProject.DAL.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbAjaxProject.DAL.Entities
 {
     public class Category
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
     }
 }
--- MongoDbAjaxProject/DAL/Entities/Product.cs
+++ MongoDbAjaxProject/DAL/Entities/Product.cs
@@ -1,6 +1,17 @@
-﻿namespace MongoDbAjaxProject.DAL.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbAjaxProject.DAL.Entities
 {
     public class Product
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductStock { get; set; }
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal ProductPrice { get; set; }
+        public string CategoryID { get; set; }
     }
 }
