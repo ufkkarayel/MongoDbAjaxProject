612d7b7d8d81ef9e2aa056798ad972d22b9b2d53
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-13 20:16:33 +0300

    employee controller düzenlendi

--- MongoDbAjaxProject/Controllers/EmployeeController.cs
+++ MongoDbAjaxProject/Controllers/EmployeeController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDbAjaxProject.DAL.Entities;
+using MongoDbAjaxProject.DAL.Settings;
 
 namespace MongoDbAjaxProject.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly IMongoCollection<Employee> _employeeCollection;
+        public EmployeeController(IDatabaseSettings _databaseSettings)
+        {
+            var client= new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _employeeCollection = database.GetCollection<Employee>(_databaseSettings.EmployeeCollectionName);
+        }
         public IActionResult Index()
         {
             return View();
