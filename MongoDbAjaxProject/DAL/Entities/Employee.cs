385a917a10c50d02c8e1c1e1b8142a7576de81f7
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-13 20:10:44 +0300

    employee sınıfı oluşturuldu

---
+++ MongoDbAjaxProject/DAL/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbAjaxProject.DAL.Entities
+{
+    public class Employee
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeSurname { get; set; }
+        public decimal EmployeeSalary { get; set; }
+    }
+}
